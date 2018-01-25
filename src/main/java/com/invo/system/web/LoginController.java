92c2c8a236fd81cc9fc406f4dbe773117436bb2f
wuwg <wuwanggao@pinming.cn>
2018-01-25 15:36:38 +0800

    thymeleaf include use.

--- src/main/java/com/invo/system/web/LoginController.java
+++ src/main/java/com/invo/system/web/LoginController.java
@@ -2,14 +2,19 @@ package com.invo.system.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author wuwanggao@pinming.cn
  * @dete 2018-1-24
  */
 @Controller
-@RequestMapping("/system/login")
 public class LoginController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String index() {
+        return "login";
+    }
 
 
 }
